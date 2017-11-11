e0ec8653e8c74a25938f308b67da9f66f87bfea6
Stefan Esterer <stefan.esterer@gmail.com>
2017-11-11 13:04:02 +0100

    * Handle Permission Denied

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -88,6 +88,13 @@ pushButton.addEventListener('click', function() {
 }
 
 function updateBtn() {
+  if (Notification.permission === 'denied') {
+    pushButton.textContent = 'Push Messaging Blocked.';
+    pushButton.disabled = true;
+    updateSubscriptionOnServer(null);
+    return;
+  }
+
   if (isSubscribed) {
     pushButton.textContent = 'Disable Push Messaging';
   } else {
